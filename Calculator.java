562dfb26afdc472b9f34ecd4f56a145486f5f578
hisame103018 <hisame103018@gmail.com>
2023-12-14 15:45:52 +0900

    test

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 	int my_add(int number1, int number2){
-        	if (number1 + number2 < 0)
-                	return;
-                return number1+number2;
-    	}
+           if (number1 + number2 < 0)
+                return;
+           return number1+number2;
+	}    
     }
 }
